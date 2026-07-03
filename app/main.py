cee8e0580cb14373b5a11e6b1d23b2e9ba402830
mame-penda-fall <mpf92005@gmail.com>
2026-07-03 18:31:48 +0000

    API utilisateurs + clients + stabilisation backend

--- app/main.py
+++ app/main.py
@@ -2,20 +2,23 @@ from flask import Flask, jsonify
 from flask_socketio import SocketIO
 import os
 
-from routes.produits import produits_bp
-
 app = Flask(__name__)
-app.register_blueprint(produits_bp)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'test')
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+# IMPORT DES ROUTES (APRES app)
+from routes.produits import produits_bp
+from routes.categories import categories_bp
+from routes.utilisateurs import utilisateurs_bp
+app.register_blueprint(utilisateurs_bp)
+app.register_blueprint(produits_bp)
+app.register_blueprint(categories_bp)
 
 @app.route('/')
 def home():
     return jsonify({"status": "OK API LIVGUINAR"})
-
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
